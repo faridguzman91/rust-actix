581d9bd83732875d4f058f802e4f0151266ef690
Farid Guzman <ghostrider_x101@msn.com>
2025-03-02 23:18:52 +0100

    added comments

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use actix_web::{ get, patch, post, App, HttpResponse, HttpServer, Responder };
 
-//actic_Web get macro
+//actic_Web get macro handler to get_pizzas
 #[get("/pizzas")]
 async fn get_pizzas() -> impl Responder {
+    //return 200 ok with string body
     HttpResponse::Ok().body("Pizzas available")
 }
 
@@ -10,8 +11,10 @@ async fn get_pizzas() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
+        //register get pizzas route
         App::new()
              .service(get_pizzas)
     })
+        // bind to localhost 8080
         .bind("127.0.0.1:8080")?.run().await
 }
