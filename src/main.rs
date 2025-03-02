a752e97fe91d67bd00533adeb9b774e2756f2dd6
Farid Guzman <ghostrider_x101@msn.com>
2025-03-02 22:55:37 +0100

    added simple http server

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-use actix_web::{ get, post, patch, Responder, HttpResponse };
+use actix_web::{ get, patch, post, App, HttpResponse, HttpServer, Responder };
 
+//actic_Web get macro
 #[get("/pizzas")]
 async fn get_pizzas() -> impl Responder {
     HttpResponse::Ok().body("Pizzas available")
 }
 
-fn main() {
-    println!("Hello, world!");
+//actix_web macro
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+             .service(get_pizzas)
+    })
+        .bind("127.0.0.1:8080")?.run().await
 }
