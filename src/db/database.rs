82054e5ea6bda7973df4ed224683dfe09fe05c98
faridguzman91 <farid.guzman91@hotmail.com>
2025-03-26 22:41:47 +0100

    added wss

--- src/db/database.rs
+++ src/db/database.rs
@@ -11,8 +11,18 @@ pub struct Database {
 
 impl Database {
     pub async fn init() -> Result<Self, Error> {
-
-        let client = Surreal::new::<Ws>("127.0.0.1");
+        let client = Surreal::new::<Ws>("127.0.0.1:8000").await?;
+        clinet
+            .signin(Root {
+                username: "root",
+                password: "root",
+            })
+            .await?;
+        client.use_ns("surreal").use_db("pizzas").await.unwrap();
+        Ok(Database {
+            client,
+            name_space: String::from("surreal"),
+            db_name: String::from("pizzas")
+        })
     }
 }
-
