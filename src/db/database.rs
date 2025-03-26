4c43713a1eff855155d90c65f3001fc15942963d
faridguzman91 <farid.guzman91@hotmail.com>
2025-03-26 21:45:27 +0100

    added bacon

---
+++ src/db/database.rs
@@ -0,0 +1,18 @@
+use surrealdb::engine::remote::ws::{Client, Ws};
+use surrealdb::opt::auth::Root;
+use surrealdb::{Error, Surreal};
+
+#[derive(Clone)]
+pub struct Database {
+    pub client: Surreal<Client>,
+    pub name_space: String,
+    pub db_name: String,
+}
+
+impl Database {
+    pub async fn init() -> Result<Self, Error> {
+
+        let client = Surreal::new::<Ws>("127.0.0.1");
+    }
+}
+
